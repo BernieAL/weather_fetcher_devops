8aec91cb04fa54d10c68afb4cd42be8e8bfc35f6
ubuntu <ubuntu@ubuntu50gb.myguest.virtualbox.org>
2024-12-26 17:37:42 -0500

    fixing test case

--- services/weather_fetcher/test_app.py
+++ services/weather_fetcher/test_app.py
@@ -1,15 +1,24 @@
 from app import app
 import pytest
+from unittest.mock import patch 
+
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+    
+#we mock the requests.get function
+@patch('requests.get')
+def test_fetch_weather(mock_get,client):
+    #setup mock response
 
-def test_fetch_weather(client):
+    mock_get.return_value.json.return_value = {
+        "main": {"temp":20},
+        "weather": [{"description": "clear"}],
+        "wind":{"speed":5}
+    }
+
     response = client.get('/fetch/london')
     assert response.status_code == 200
-    data = response.get_json()
-    assert 'temperature' in data
-
